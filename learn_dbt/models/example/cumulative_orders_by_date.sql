d0dc930c69da978c54fcdbcce6666eab68887b28
Janko <janko.chavannes@riviq.nl>
2023-12-07 12:51:34 +0100

    Add assignment 1

---
+++ learn_dbt/models/example/cumulative_orders_by_date.sql
@@ -0,0 +1,23 @@
+{{ config(materialized='table') }}
+
+WITH orders AS (
+    SELECT o_orderdate as order_date
+        , o_totalprice as price
+    FROM snowflake_sample_data.tpch_sf1.orders
+)
+, order_value_by_date AS (
+    SELECT
+        order_date
+        , SUM(price) AS total_value
+    FROM orders
+    GROUP BY 1
+)
+, cumulative_value_by_date AS (
+    SELECT
+        order_date
+        , SUM(total_value) OVER (ORDER BY order_date ASC) AS cumulative_value
+    FROM order_value_by_date
+)
+SELECT *
+FROM cumulative_value_by_date
+ORDER BY order_date
